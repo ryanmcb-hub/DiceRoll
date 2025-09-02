d36e264ca058399e46eeb480f17147cf8d004a68
ryanmcb <ryanmcb@umich.edu>
2025-09-01 21:13:01 -0400

    First Dicetest code test

--- main.cpp
+++ main.cpp
@@ -20,15 +20,15 @@ int main()
         // Print rolls
         cout << d1 << " " << d2 << " " << d3 << " " << d4 << " " << d5 << endl;
 
-        // Tally counts
-        int tally[7] = {0}; // index 1–6 will be used
+        // Tally counts creates an array with 7 elements, all starting at 0 Every time a number appears increment spot in array
+        int tally[7] = {0};
         tally[d1]++;
         tally[d2]++;
         tally[d3]++;
         tally[d4]++;
         tally[d5]++;
 
-        // Print tally results
+        // Print tally results face takes on values 1-6. if tally[face] > 0 means at least one die showed that number
         for (int face = 1; face <= 6; face++)
         {
             if (tally[face] > 0)
